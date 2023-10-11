dd74af31a5f3b3f2e597932c132ed5963befef0f
R1Vix <ericcfan@yahoo.com>
2023-10-11 10:12:22 +0800

    Initial Commit

---
+++ Proj_1011/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Proj_1011
+//
+//  Created by user14 on 2023/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
